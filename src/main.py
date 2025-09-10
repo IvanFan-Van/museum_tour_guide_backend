25235495ee249249fe9e1de81ea47be9cb7b7974
ivan <ivanfan@connect.hku.hk>
2025-09-10 23:57:24 +0800

    添加 cors 支持

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 from typing import TypedDict
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from langchain_core.messages import HumanMessage
 
 from src.agent.graph import graph
 
+origins = ["*"]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class QueryRequest(TypedDict):
